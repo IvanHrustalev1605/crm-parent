20c0bc4d452c8f0dfcf404d52b28aeaa53601433
khrustalev <khrustalev16@bk.ru>
2024-03-24 18:06:35 +0300

    xzbm6776b

--- storage-service/src/main/kotlin/com/khrustalev/storageservice/StorageServiceApplication.kt
+++ storage-service/src/main/kotlin/com/khrustalev/storageservice/StorageServiceApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
 @SpringBootApplication
-@EnableElasticsearchRepositories
+//@EnableElasticsearchRepositories
 class StorageServiceApplication
 
 fun main(args: Array<String>) {
